199e6bec5e69ce5570b9e5ea850f5c059b5b58a4
Kaori Kawakami <k.kawakami08@gmail.com>
2023-05-04 18:26:30 -0700

    initial commit, Add project styling/font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        veryLightGrayishBlue: "#f7fafd",
+        lightGrayishBlueOne: "#e5effa",
+        lightGrayishBlueTwo: "#dde7ee",
+        grayishBlue: "#939dae",
+        darkGrayishBlue: "#5e6778",
+        veryDarkBlue: "#1c202b",
       },
     },
   },
   plugins: [],
-}
+};
